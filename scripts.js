998997a24353ea1c7eec66ae1b0dbdfca09d4c89
JINSUP SONG <jinsup.song@my.jcu.edu.au>
2017-05-22 10:41:12 +0800

    update
    html updates
    javascript updates

--- scripts.js
+++ scripts.js
@@ -11,15 +11,14 @@ function resetForm() {
 }
 
 
- function myCalc()
- {
+ function myCalc() {
 	var ItemCode = document.getElementById("ItemCode");
 	var quantity = document.getElementById("quantity");
 	var ttl = document.getElementById("total");
 	
 	if (ItemCode[ItemCode.selectedIndex].value !="" && quantity[quantity.selectedIndex].value !="")
 	{
-		ttl.value = (ItemCode[ItemCode.selectedIndex].value * quantity[quantity.selectedIndex].value);
+		ttl.value = (ItemCode[ItemCode.selectedIndex].value * quantity[quantity.selectedIndex].value).toFixed(2);
 	}
  }
  
